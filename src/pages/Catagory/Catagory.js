46c2364492ab35b411fbb7959fd557c61cd1b95a
Authoi234 <jawadkhan.authoi@gmail.com>
2023-09-26 16:53:57 +0630

    category wise news is set

--- src/pages/Catagory/Catagory.js
+++ src/pages/Catagory/Catagory.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummuryCard from '../Shared/NewsSummuryCard/NewsSummuryCard';
 
 const Catagory = () => {
-    const news = useLoaderData();
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h2>This is Category has news: {news.length}</h2>    
+            <h2>This is Category has news: {categoryNews.length}</h2>    
+            {
+                categoryNews.map(news => <NewsSummuryCard key={news._id} news={news}></NewsSummuryCard>)
+            }
         </div>
     );
 };
