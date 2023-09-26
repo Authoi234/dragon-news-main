f37b038024f7b6a0bc212875888560d496d11ff7
Authoi234 <jawadkhan.authoi@gmail.com>
2023-09-26 16:51:22 +0630

    home is full by news

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummuryCard from '../Shared/NewsSummuryCard/NewsSummuryCard';
 
 const Home = () => {
+    const allNews = useLoaderData();
     return (
         <div>
-            <h2> This is home</h2>
+            <h2>Dragon News Home: {allNews.length}</h2>
+            {
+                allNews.map(news => <NewsSummuryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSummuryCard>)
+            }
         </div>
     );
 };
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -4,13 +4,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary mb-4" bg='light' variant='light'>
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href=""><Link to="/">Dragon News</Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
